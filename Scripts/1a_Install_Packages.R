87a07c2427a05ac2dcf98c09f84b68d3e9620096
dwh77 <dwh1998@vt.edu>
2025-01-30 11:23:31 -0500

    rerun 4casts w/ longer horizon and duration

--- Scripts/1a_Install_Packages.R
+++ Scripts/1a_Install_Packages.R
@@ -8,5 +8,9 @@ install.packages('imputeTS') # got a notice that this was needed for Met regress
 install.packages('zoo') # got a notice that this was needed for RainLag model function script - is that actually true?
 install.packages('fable') # needed for NNETAR
 install.packages('urca') # needed for NNETAR
+install.packages('aws.s3') 
+install.packages('tsibble') 
+install.packages('fable') 
+
 
 devtools::install_github("eco4cast/score4cast")
